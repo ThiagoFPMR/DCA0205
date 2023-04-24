546898a2bf7a51df64bcab11ace0f877e157a5a0
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2023-04-24 14:56:25 -0300

    first commit

---
+++ DataGenerator.py
@@ -0,0 +1,39 @@
+from abc import ABC
+
+
+class DataGenerator(ABC):
+    def __init__(self, páth):
+        pass
+
+    def __iter__(self) -> dict:
+        pass
+
+
+import csv
+
+
+class csvDataGenerator(DataGenerator):
+    def __init__(self, path):
+        super().__init__(path)
+        self.csv_path = path
+
+    def __iter__(self) -> dict:
+        with open(self.csv_path, "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                yield row
+
+
+import json
+
+
+class jsonDataGenerator(DataGenerator):
+    def __init__(self, path):
+        super().__init__(path)
+        self.json_path = path
+
+    def __iter__(self) -> dict:
+        with open(self.json_path, "r") as f:
+            data = json.load(f)["results"]
+            for row in data:
+                yield row
