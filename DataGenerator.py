010b31bfdd9706a62316386555233658ec65e0b1
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2023-04-24 18:07:25 -0300

    changes to make explanations easier

--- DataGenerator.py
+++ DataGenerator.py
@@ -2,38 +2,22 @@ from abc import ABC
 
 
 class DataGenerator(ABC):
-    def __init__(self, páth):
+    def __init__(self, file):
         pass
 
     def __iter__(self):
         pass
 
 
-import csv
-
-
-class csvDataGenerator(DataGenerator):
-    def __init__(self, path):
-        super().__init__(path)
-        self.csv_path = path
-
-    def __iter__(self) -> dict:
-        with open(self.csv_path, "r") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                yield row
-
-
 import json
 
 
 class jsonDataGenerator(DataGenerator):
-    def __init__(self, path):
-        super().__init__(path)
-        self.json_path = path
+    def __init__(self, file):
+        super().__init__(file)
+        self.json_file = file
 
     def __iter__(self) -> dict:
-        with open(self.json_path, "r") as f:
-            data = json.load(f)["results"]
-            for row in data:
-                yield row
+        data = json.load(self.json_file)["results"]
+        for row in data:
+            yield row
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from DataGenerator import DataGenerator, csvDataGenerator, jsonDataGenerator
+import csv
+from DataGenerator import DataGenerator, jsonDataGenerator
 
 csv_path, json_path = "cafes.csv", "cafes.json"
 
 
 def main(dataGenerator: DataGenerator, path: str) -> int:
     count = 0
-    for row in dataGenerator(path):
-        if float(row["rating"]) > 4:  # establecimentos com rating maior que 4
-            count += 1
+    with open(path, "r") as f:
+        for row in dataGenerator(f):
+            if float(row["rating"]) > 4:  # establecimentos com rating maior que 4
+                count += 1
     return count
 
 
-print("out usando csv:", main(csvDataGenerator, csv_path))
+print("out usando csv:", main(csv.DictReader, csv_path))
 print("--------------------")
 print("out usando json:", main(jsonDataGenerator, json_path))
